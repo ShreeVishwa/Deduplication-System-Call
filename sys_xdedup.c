f2db473eac4d64c76ba3bd540a5a4a8f5683f410
vishwatej reddy <reddyvishwatej176@gmail.com>
2018-07-03 13:20:24 -0400

    Done implementing the deduplication system call

--- sys_xdedup.c
+++ sys_xdedup.c
@@ -17,7 +17,7 @@ asmlinkage extern long (*sysptr)(void *arg);
 
 asmlinkage long xdedup(void *arg)
 {
-	struct Param* param ;//= (struct Param*)kmalloc(sizeof(struct Param),GFP_KERNEL);
+	struct Param* param; //= (struct Param*)kmalloc(sizeof(struct Param),GFP_KERNEL);
 	struct file *filp1;
 	struct file *filp2;
 	struct file *filp3;
@@ -53,8 +53,8 @@ asmlinkage long xdedup(void *arg)
 	unsigned short out_mode = 0644;
 	int d_flag;
 	int copy_user;
-	
-		
+
+
 	//char buff3[20];
 	param = (struct Param*)arg;//kmalloc(sizeof(struct Param*),GFP_KERNEL);
 	/*copy_user = copy_from_user(param,arg,sizeof(param));
@@ -110,19 +110,19 @@ asmlinkage long xdedup(void *arg)
 			if(!S_ISREG(stats2.mode)){
 				printk("Input file 2 is not a regular file\n");
 				return -EBADF;
-			}	
-			
+			}
+
 			if(stats1.uid.val != stats2.uid.val){
 				printk("The files belong to different owners so can't dedup\n");
 				return -EPERM;
 			}
-	
+
 			kern_path_sb1 = kern_path(param->f1name,LOOKUP_FOLLOW,&path_sb1);
 			if(kern_path_sb1 != 0){
 				printk("Error getting the path for the file 1\n");
 				return -kern_path_sb1;
 			}
-					
+
 			kern_path_sb2 = kern_path(param->f2name,LOOKUP_FOLLOW,&path_sb2);
 			if(kern_path_sb2 != 0){
 				printk("Error getting the path for the file 2\n");
@@ -148,8 +148,8 @@ asmlinkage long xdedup(void *arg)
 		}
 
 		while(file_size < stats1.size){
-			
-			buff3 = (char*)kmalloc(sizeof(char)*4096,GFP_KERNEL); 	
+
+			buff3 = (char*)kmalloc(sizeof(char)*4096,GFP_KERNEL);
 			buff4 = (char*)kmalloc(sizeof(char)*4096,GFP_KERNEL);
 
 
@@ -160,11 +160,11 @@ asmlinkage long xdedup(void *arg)
     			if (!filp1 || IS_ERR(filp1)) {
 				printk("Reading file 1 is failed\n");
 				printk("wrapfs_read_file err %d\n", (int) PTR_ERR(filp1));
-				return -(int)PTR_ERR(filp1);  // or do something else 
-    			}	
+				return -(int)PTR_ERR(filp1);  // or do something else
+    			}
 			//printk("No error in file1 pointer\n");
-    			// now read len bytes from offset 0 
-			filp1->f_pos = file_size;// start offset 
+    			// now read len bytes from offset 0
+			filp1->f_pos = file_size;// start offset
     			oldfs1 = get_fs();
     			set_fs(KERNEL_DS);
     			bytes1 = vfs_read(filp1, buff3,4096, &filp1->f_pos);
@@ -172,7 +172,7 @@ asmlinkage long xdedup(void *arg)
 			//printk("%d\n",bytes1);
 			//printk("%s\n",buff3);
 
-    			// close the file 
+    			// close the file
     			filp_close(filp1, NULL);
 			filp2 = filp_open(param->f2name,O_RDONLY,0);
 			//printk("Done reading file 2\n");
@@ -202,7 +202,7 @@ asmlinkage long xdedup(void *arg)
 
 			//printk("Done reading file\n");
 			//printk("current size is %d i is %d",file_size,i);
-			
+
 			if(i < 4096 && file_size + i != stats1.size){
 
 				if(param->b1 & FLAG_P){
@@ -212,12 +212,12 @@ asmlinkage long xdedup(void *arg)
 				printk("The files are not identical\n");
 				return -1001;
 			}
-			
-			
+
+
 			file_size += bytes1;
 			kfree(buff4);
 			kfree(buff3);
-		
+
 			//printk("Number of bytes read are %d\n",bytes1);
 
 
@@ -231,9 +231,9 @@ asmlinkage long xdedup(void *arg)
 
 			return file_size;
 
-		} 
+		}
 
-		
+
 		else if((param->b1 & FLAG_P) && !(param->b1 & FLAG_N)){
 
 			error1 = vfs_stat(param->f1name,&stats1);
@@ -258,7 +258,7 @@ asmlinkage long xdedup(void *arg)
 					printk("Output file doesn't have write permission\n");
 					return -EPERM;
 				}
-			
+
 				kern_path_sb3 = kern_path(param->outfile,LOOKUP_FOLLOW,&path_sb3);
 				if(kern_path_sb3 != 0){
 					printk("Error getting the path\n");
@@ -274,7 +274,7 @@ asmlinkage long xdedup(void *arg)
 				printk("Output file is already hardlinked with the input file 1\n");
 				return -1003;
 				}
-	
+
 				if(stats3.ino == stats2.ino && memcmp(path_sb3.dentry->d_inode->i_sb->s_uuid,path_sb2.dentry->d_inode->i_sb->s_uuid,sizeof(path_sb2.dentry->d_inode->i_sb->s_uuid))==0){
 				printk("Output file is already hardlinked with the input file 2\n");
 				return -1003;
@@ -291,19 +291,19 @@ asmlinkage long xdedup(void *arg)
 			if(!S_ISREG(stats2.mode)){
 				printk("Input file 2 is not a regular file\n");
 				return -EBADF;
-			}	
-			
+			}
+
 			if(stats1.uid.val != stats2.uid.val){
 				printk("The files belong to different owners so can't dedup\n");
 				return -EPERM;
 			}
-	
+
 			kern_path_sb1 = kern_path(param->f1name,LOOKUP_FOLLOW,&path_sb1);
 			if(kern_path_sb1 != 0){
 				printk("Error getting the path for the file 1\n");
 				return -kern_path_sb1;
 			}
-					
+
 			kern_path_sb2 = kern_path(param->f2name,LOOKUP_FOLLOW,&path_sb2);
 			if(kern_path_sb2 != 0){
 				printk("Error getting the path for the file 2\n");
@@ -331,7 +331,7 @@ asmlinkage long xdedup(void *arg)
 			}
 			return 0;
 		}
-		
+
 		getnstimeofday(&now);
 		if(d_flag){
 			printk("File owners are same\n");
@@ -340,7 +340,7 @@ asmlinkage long xdedup(void *arg)
 		//n = snprintf(NULL,0,"%ld",now.tv_nsec);
 		temp_name = (char*)kmalloc(sizeof(char)*10,GFP_KERNEL);
 		c = snprintf(temp_name,10,"%ld",now.tv_nsec);
-		
+
 		//printk("%s\n",temp_name);
 
 		//temp_name = "tempfile.txt";
@@ -349,9 +349,9 @@ asmlinkage long xdedup(void *arg)
     		if (!filp1 || IS_ERR(filp1)) {
 			printk("Reading file 1 is failed\n");
 			printk("wrapfs_read_file err %d\n", (int) PTR_ERR(filp1));
-			return -(int)PTR_ERR(filp1);  // or do something else 
+			return -(int)PTR_ERR(filp1);  // or do something else
     		}
-		
+
 		filp1->f_pos = 0;
 		//filp_close(filp1, NULL);
 		filp2 = filp_open(param->f2name,O_RDONLY,0);
@@ -387,22 +387,22 @@ asmlinkage long xdedup(void *arg)
 			printk("A temporary file has been created\n");
 		}
 
-	
+
 
 
 		while(file_size < stats1.size){
-			
-			buff3 = (char*)kmalloc(sizeof(char)*4096,GFP_KERNEL); 	
+
+			buff3 = (char*)kmalloc(sizeof(char)*4096,GFP_KERNEL);
 			buff4 = (char*)kmalloc(sizeof(char)*4096,GFP_KERNEL);
 
-			//temp_name = "tempfile.txt";			
+			//temp_name = "tempfile.txt";
 
 			//printk("I am in while loop\n");
 
-	    			
+
 			//printk("No error in file1 pointer\n");
-    			// now read len bytes from offset 0 
-			//filp1->f_pos = file_size;// start offset 
+    			// now read len bytes from offset 0
+			//filp1->f_pos = file_size;// start offset
     			oldfs1 = get_fs();
     			set_fs(KERNEL_DS);
     			bytes1 = vfs_read(filp1, buff3, 4096, &filp1->f_pos);
@@ -410,7 +410,7 @@ asmlinkage long xdedup(void *arg)
 			//printk("%d\n",bytes1);
 			//printk("%s\n",buff3);
 
-    			// close the file 
+    			// close the file
     			//filp2->f_pos = file_size;
 			oldfs2 = get_fs();
 			set_fs(KERNEL_DS);
@@ -422,7 +422,7 @@ asmlinkage long xdedup(void *arg)
 			//filp_close(filp2,NULL);
 			//printk("Done reading from two chunks\n");
 
-			
+
 
 			i = 0;
 			while(*(buff3+i) != '\0' && i < 4096 && *(buff4+i) != '\0'){
@@ -443,20 +443,20 @@ asmlinkage long xdedup(void *arg)
 				}
 				return 0;
 			}
-	
+
 
 			//printk("Done reading file\n");
-			
+
 				//filp3->f_pos = file_size;
 			oldfs3 = get_fs();
 			set_fs(KERNEL_DS);
-	
+
 			bytes3 = vfs_write(filp3,buff3,i,&filp3->f_pos);
-			
+
 			if(i != bytes3){
 				printk("Error in writing the file\n");
 				mutex_lock_nested(&(path3.dentry)->d_parent->d_inode->i_mutex, I_MUTEX_PARENT);
-				unlink_temp_err = vfs_unlink((path3.dentry)->d_parent->d_inode,path3.dentry,NULL);	
+				unlink_temp_err = vfs_unlink((path3.dentry)->d_parent->d_inode,path3.dentry,NULL);
 				mutex_unlock(&(path3.dentry)->d_parent->d_inode->i_mutex);
 				printk("Temp file deleted\n");
 				if(unlink_temp_err){
@@ -465,22 +465,22 @@ asmlinkage long xdedup(void *arg)
 				}
 
 				return -1004;
-	
+
 			}
 
 			//filp_close(filp3,NULL);
 
-					
+
 
 			if(i < 4096 && file_size + i != stats1.size){
 				file_size = file_size + i;
 				break;
 			}
-		
+
 			file_size += bytes1;
 			kfree(buff4);
 			kfree(buff3);
-		
+
 			//printk("Number of bytes read are %d\n",bytes1);
 
 
@@ -504,7 +504,7 @@ asmlinkage long xdedup(void *arg)
 				printk("Error in getting the path name for file 1\n");
 				return -kern_temp_int1;
 			}
-	
+
 			if(kern_temp_int2){
 				printk("Error in getting the path name for file 2\n");
 				return -kern_temp_int2;
@@ -514,27 +514,27 @@ asmlinkage long xdedup(void *arg)
 			//printk("kern_temp_2 is %d\n",kern_temp_int2);
 
 			mutex_lock_nested(&(path3.dentry)->d_parent->d_inode->i_mutex, I_MUTEX_PARENT);
-	
+
 			src = lookup_one_len(temp_name,path3.dentry->d_parent,strlen(temp_name));
 			mutex_unlock(&(path3.dentry)->d_parent->d_inode->i_mutex);
-	
+
 			if(IS_ERR(src)){
 				printk("There is error in src lookup\n");
 				return((int)PTR_ERR(src));
 			}
 
 			mutex_lock_nested(&(path4.dentry)->d_parent->d_inode->i_mutex, I_MUTEX_PARENT);
-	
+
  			dst = lookup_one_len(path4.dentry->d_iname,path4.dentry->d_parent,strlen(path4.dentry->d_iname));
-			
+
 			mutex_unlock(&(path4.dentry)->d_parent->d_inode->i_mutex);
-	
-			
+
+
 			if(IS_ERR(dst)){
 				printk("There is err in dst lookup\n");
 				return ((int)PTR_ERR(dst));
 			}
-			
+
 			if(d_flag){
 				printk("src and dest dentries are taken\n");
 			}
@@ -542,10 +542,10 @@ asmlinkage long xdedup(void *arg)
 			lock_rename(src->d_parent,dst->d_parent);
 
 			vfs_ret_val = vfs_rename(path3.dentry->d_parent->d_inode,src,path4.dentry->d_parent->d_inode,dst,NULL,0);
-			
+
 			unlock_rename(src->d_parent,dst->d_parent);
 			//printk("vfs_rname done val is %d\n", vfs_ret_val);
-			
+
 			if(vfs_ret_val){
 				printk("Rename unsuccessfull\n");
 				return -vfs_ret_val;
@@ -582,7 +582,7 @@ asmlinkage long xdedup(void *arg)
 			}
 
 			if(error3 == 0){
-			
+
 				kern_path_sb3 = kern_path(param->outfile,LOOKUP_FOLLOW,&path_sb2);
 				if(kern_path_sb3 != 0){
 					printk("Error getting the path\n");
@@ -598,7 +598,7 @@ asmlinkage long xdedup(void *arg)
 				printk("Output file is already hardlinked with the input file 1\n");
 				return -1003;
 				}
-	
+
 				if(stats3.ino == stats2.ino && memcmp(path_sb3.dentry->d_inode->i_sb->s_uuid,path_sb2.dentry->d_inode->i_sb->s_uuid,sizeof(path_sb2.dentry->d_inode->i_sb->s_uuid))==0){
 				printk("Output file is already hardlinked with the input file 2\n");
 				return -1003;
@@ -615,19 +615,19 @@ asmlinkage long xdedup(void *arg)
 			if(!S_ISREG(stats2.mode)){
 				printk("Input file 2 is not a regular file\n");
 				return -EBADF;
-			}	
-			
+			}
+
 			if(stats1.uid.val != stats2.uid.val){
 				printk("The files belong to different owners so can't dedup\n");
 				return -EPERM;
 			}
-	
+
 			kern_path_sb1 = kern_path(param->f1name,LOOKUP_FOLLOW,&path_sb1);
 			if(kern_path_sb1 != 0){
 				printk("Error getting the path for the file 1\n");
 				return -kern_path_sb1;
 			}
-					
+
 			kern_path_sb2 = kern_path(param->f2name,LOOKUP_FOLLOW,&path_sb2);
 			if(kern_path_sb2 != 0){
 				printk("Error getting the path for the file 2\n");
@@ -657,12 +657,12 @@ asmlinkage long xdedup(void *arg)
 		//printk("%s\n",temp_name);
 		//temp_name = "tempfile.txt";
 		//temp_name = "tempfile.txt";
-		
-	
+
+
 
 		while(file_size < stats1.size){
-			
-			buff3 = (char*)kmalloc(sizeof(char)*4096,GFP_KERNEL); 	
+
+			buff3 = (char*)kmalloc(sizeof(char)*4096,GFP_KERNEL);
 			buff4 = (char*)kmalloc(sizeof(char)*4096,GFP_KERNEL);
 
 
@@ -673,11 +673,11 @@ asmlinkage long xdedup(void *arg)
     			if (!filp1 || IS_ERR(filp1)) {
 				printk("Reading file 1 is failed\n");
 				printk("wrapfs_read_file err %d\n", (int) PTR_ERR(filp1));
-				return -(int)PTR_ERR(filp1);  // or do something else 
-    			}	
+				return -(int)PTR_ERR(filp1);  // or do something else
+    			}
 			//printk("No error in file1 pointer\n");
-    			// now read len bytes from offset 0 
-			filp1->f_pos = file_size;// start offset 
+    			// now read len bytes from offset 0
+			filp1->f_pos = file_size;// start offset
     			oldfs1 = get_fs();
     			set_fs(KERNEL_DS);
     			bytes1 = vfs_read(filp1, buff3, 4096, &filp1->f_pos);
@@ -685,7 +685,7 @@ asmlinkage long xdedup(void *arg)
 			//printk("%d\n",bytes1);
 			//printk("%s\n",buff3);
 
-    			// close the file 
+    			// close the file
     			filp_close(filp1, NULL);
 			filp2 = filp_open(param->f2name,O_RDONLY,0);
 			//printk("Done reading file 2\n");
@@ -715,7 +715,7 @@ asmlinkage long xdedup(void *arg)
 
 			//printk("Done reading file\n");
 			//printk("current size is %d i is %d",file_size,i);
-			
+
 			filp3 = filp_open(temp_name,O_WRONLY|O_CREAT,0644);
 			//printk("Done reading file\n");
 			if(!filp3 || IS_ERR(filp3)){
@@ -732,10 +732,10 @@ asmlinkage long xdedup(void *arg)
 
 
 			if(i < 4096 && file_size + i != stats1.size){
-				
+
 				//printk("The files are not identical\n");
 				mutex_lock_nested(&(path3.dentry)->d_parent->d_inode->i_mutex, I_MUTEX_PARENT);
-				unlink_temp_err = vfs_unlink((path3.dentry)->d_parent->d_inode,path3.dentry,NULL);	
+				unlink_temp_err = vfs_unlink((path3.dentry)->d_parent->d_inode,path3.dentry,NULL);
 				mutex_unlock(&(path3.dentry)->d_parent->d_inode->i_mutex);
 				//printk("Temp file deleted\n");
 				if(unlink_temp_err){
@@ -749,17 +749,17 @@ asmlinkage long xdedup(void *arg)
 			filp3->f_pos = file_size;
 			oldfs3 = get_fs();
 			set_fs(KERNEL_DS);
-	
+
 			bytes3 = vfs_write(filp3,buff3,i,&filp3->f_pos);
 
 			filp_close(filp3,NULL);
 
 
-			
+
 			file_size += bytes1;
 			kfree(buff4);
 			kfree(buff3);
-		
+
 			//printk("Number of bytes read are %d\n",bytes1);
 
 
@@ -778,13 +778,13 @@ asmlinkage long xdedup(void *arg)
 				return -1006;
 			}
 			mutex_lock_nested(&(path2.dentry)->d_parent->d_inode->i_mutex, I_MUTEX_PARENT);
-			unlink_err1 = vfs_unlink((path2.dentry)->d_parent->d_inode,path2.dentry,NULL);	
+			unlink_err1 = vfs_unlink((path2.dentry)->d_parent->d_inode,path2.dentry,NULL);
 			mutex_unlock(&(path2.dentry)->d_parent->d_inode->i_mutex);
-			
+
 			if(unlink_err1){
 				printk("Unlink of file 2 failed\n");
 				mutex_lock_nested(&(path3.dentry)->d_parent->d_inode->i_mutex, I_MUTEX_PARENT);
-				unlink_temp_err = vfs_unlink((path3.dentry)->d_parent->d_inode,path3.dentry,NULL);	
+				unlink_temp_err = vfs_unlink((path3.dentry)->d_parent->d_inode,path3.dentry,NULL);
 				mutex_unlock(&(path3.dentry)->d_parent->d_inode->i_mutex);
 				if(unlink_temp_err){
 					printk("Unlinking with the temp file also failed\n");
@@ -792,7 +792,7 @@ asmlinkage long xdedup(void *arg)
 				}
 				return -unlink_err1;
 
-			}	
+			}
 
 			//printk("unlink error is %d\n",unlink_err1);
 			//
@@ -811,42 +811,42 @@ asmlinkage long xdedup(void *arg)
 			link_err1 = vfs_link(path1.dentry,path1.dentry->d_parent->d_inode,link_dentry,NULL);
 			//printk("kern_int1 : %d\n",kern_int1);
 			//printk("link_err1: %d\n",link_err1);
-			
+
 
 			if(link_err1){
 				mutex_lock_nested(&(path3.dentry)->d_parent->d_inode->i_mutex, I_MUTEX_PARENT);
-	
+
 			src = lookup_one_len(temp_name,path3.dentry->d_parent,strlen(temp_name));
 			mutex_unlock(&(path3.dentry)->d_parent->d_inode->i_mutex);
-	
+
 			if(IS_ERR(src)){
 				printk("There is error in src lookup\n");
 				return((int)PTR_ERR(src));
 			}
 
 			mutex_lock_nested(&(path2.dentry)->d_parent->d_inode->i_mutex, I_MUTEX_PARENT);
-	
+
  			dst = lookup_one_len(path2.dentry->d_iname,path2.dentry->d_parent,strlen(path2.dentry->d_iname));
-			
+
 			mutex_unlock(&(path2.dentry)->d_parent->d_inode->i_mutex);
-	
-			
+
+
 			if(IS_ERR(dst)){
 				printk("There is err in dst lookup\n");
 				return ((int)PTR_ERR(dst));
 			}
-	
+
 			printk("src and dest are done\n");
 
 			lock_rename(src->d_parent,dst->d_parent);
 
 			vfs_ret_val = vfs_rename(path3.dentry->d_parent->d_inode,src,path2.dentry->d_parent->d_inode,dst,NULL,0);
-			
+
 			unlock_rename(src->d_parent,dst->d_parent);
 			//printk("vfs_rname done val is %d\n", vfs_ret_val);
 
 			if(vfs_ret_val){
-				
+
 				printk("VFS_Rename Failed and the file 2 is lost\n");
 				return -vfs_ret_val;
 			}
@@ -863,18 +863,18 @@ asmlinkage long xdedup(void *arg)
 			}
 
 			mutex_lock_nested(&(path3.dentry)->d_parent->d_inode->i_mutex, I_MUTEX_PARENT);
-			unlink_temp_err = vfs_unlink((path3.dentry)->d_parent->d_inode,path3.dentry,NULL);	
+			unlink_temp_err = vfs_unlink((path3.dentry)->d_parent->d_inode,path3.dentry,NULL);
 			mutex_unlock(&(path3.dentry)->d_parent->d_inode->i_mutex);
 			if(unlink_temp_err){
 				printk("Unlinking with the temp file alos failed\n");
 				return -unlink_temp_err;
 			}
-			
+
 			return file_size;
 
-		
+
 		}
-		
+
 		return 0;
 
 	}
